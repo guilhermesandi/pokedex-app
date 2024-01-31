649b7f6fcbd7c0531ab1171d5894da1e9f806c3a
Guilherme Sandi <guilherme_sandi@hotmail.com>
2024-01-30 22:42:44 -0300

    Eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  extends: ["universe/native"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "function-declaration",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@components/**",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@styles/**",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
